cae5fcba5847bc1cff3073ed2f9f671b2b3611be
Dmitry <32417159+iDm1try@users.noreply.github.com>
2017-10-14 15:23:39 +0300

    Delete basic.java

--- basic.java
+++
@@ -1,8 +0,0 @@
-package vares;
-
-public class basic {
-	public static int age;
-	public static String nameP;
-	public static boolean activeLicense;
-	public static String names[] = {"Dmitry", "Slava", "Ivan"};
-}
